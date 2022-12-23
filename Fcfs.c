1c72735399695dbbfb3f5a57a059130a2b95ed09
theGhostick <96113787+theGhostick@users.noreply.github.com>
2022-12-23 01:51:47 -0800

    Create Fcfs.c

---
+++ Fcfs.c
@@ -0,0 +1,27 @@
+#include <iostream>
+#ncludee <conio.h>
+#include <fstream>
+#include <unistd.h>
+#include <getopt.h>
+
+using namespace std;
+
+int main(){
+
+int getopt(int argc, char * const argv[],
+           const char *optstring);
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+  
+  
+argv[1] = input.txt;
+
+
+
+
+
+
+
+
+return 0;}
